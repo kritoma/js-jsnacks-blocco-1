e620e4337653522eef71f1762a8cc9844f05c45f
kritoma <toma.kristian97@gmail.com>
2022-05-04 11:35:24 +0200

    snack-1

---
+++ snack-1/main.js
@@ -0,0 +1,12 @@
+// chiedere il primo numero
+const firstNumber = Number(prompt("inserisci un numero"));
+// chiedere il secondo numero
+const secondNumber = Number(prompt("inserisci un altro numero"));
+
+if ( firstNumber > secondNumber) {
+    alert(firstNumber)
+} else if (firstNumber < secondNumber ) {
+    alert(secondNumber)
+} else {
+    alert("pari")
+}
