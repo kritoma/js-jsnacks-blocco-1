ae9aa23af300d2e824259f9a3d8be9785e2bf26b
kritoma <toma.kristian97@gmail.com>
2022-05-04 12:54:00 +0200

    modifiche snack-5

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,9 +1,7 @@
 const list = [];
 
-let number;
-
 for (let i = 0; i < 6; i++) {
-    number = Number(prompt("inserisci un numero"));
+    let number = Number(prompt("inserisci un numero"));
      if ((number % 2) == 1) {
          list.push(number)
      };
