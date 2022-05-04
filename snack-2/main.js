d5d52ceebda233ade9f7ed494502ca9dcdf50eac
kritoma <toma.kristian97@gmail.com>
2022-05-04 12:25:17 +0200

    snack-3

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,7 +4,7 @@ const secondWord = prompt("inserisci la seconda parola");
 if ( firstWord.length < secondWord.length) {
     console.log(firstWord + ' ' + secondWord)
 } else if (firstWord.length == secondWord.length) {
-    console.log("sono uguali")
+    console.log("hanno la stessa lunghezza")
 } else {
     console.log(secondWord + ' ' + firstWord)
 }
---
+++ snack-3/main.js
@@ -0,0 +1,9 @@
+
+let numbers 
+
+for (let i = 0; i < 10; i++) {
+    numbers = Number(prompt("inserisci un numero"))
+}
+
+ let somma = numbers + numbers + numbers + numbers + numbers + numbers + numbers + numbers +numbers + numbers
+console.log(somma)
